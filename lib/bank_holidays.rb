f82f654a8c1062dfa3617ac80891fb58d5472030
Richard Patching <richard@justaddpixels.com>
2013-04-10 09:05:08 +0000

    Return a bank holidays

--- lib/bank_holidays.rb
+++ lib/bank_holidays.rb
@@ -1,5 +1,22 @@
+require "httparty"
+require "icalendar"
 require "bank_holidays/version"
 
 module BankHolidays
-  # Your code goes here...
+  DEFAULT_URL = "https://www.gov.uk/bank-holidays/england-and-wales.ics"
+  def self.all(url=DEFAULT_URL)
+    holidays = HTTParty.get(url)
+    calendar = Icalendar.parse(holidays)
+    calendar.first.events.map do |holiday|
+      BankHoliday.new(holiday.dtstart, holiday.summary)
+    end
+  end
+
+  class BankHoliday
+    attr_reader :date, :summary
+    def initialize(date, summary='')
+      @date = date
+      @summary = summary
+    end
+  end
 end
